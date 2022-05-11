0c764801d734ecb8fbd0c741978820570ee88086
tvishapandya <tvishapandya02@gmail.com>
2022-05-11 16:03:30 +0100

    change ins test files

--- src/test/java/aboutCompanyTest.java
+++ src/test/java/aboutCompanyTest.java
@@ -2,7 +2,9 @@ import Pages.aboutCompanyPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
